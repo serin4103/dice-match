55de0958d5d69f12d433d8b7b462e7ec31894354
serin4103 <serin4103@gmail.com>
2025-07-16 01:03:23 +0900

    refactor: type.ts copy

--- server/src/socket/socketHandler.ts
+++ server/src/socket/socketHandler.ts
@@ -1,6 +1,6 @@
 import { Server, Socket } from "socket.io";
 import { GameManager } from "../game/gameManager";
-import { MatchedEvent, Animation, PawnsMovedEvent } from "../../../types/game";
+import { MatchedEvent, Animation, PawnsMovedEvent } from "../../types/game";
 
 type PlayerInfo = { socketId: string; userId: string };
 
