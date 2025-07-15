de9846c3876bdaaa2689261210657232aa664536
serin4103 <serin4103@gmail.com>
2025-07-15 17:09:24 +0900

    refactor: Refactor game components and remove unused files

--- components/game/GameRight.tsx
+++ components/game/GameRight.tsx
@@ -20,26 +20,8 @@ export default function GameRight() {
 
     return (
         <div className={styles.gameRight}>
-            <PlayerStatus
-                name={playersState[0].name}
-                diceResult={playersState[0].diceResult}
-                color={playersState[0].color}
-                finishedCount={
-                    playersState[0].pawnsState.filter(
-                        (pawn) => pawn.position === "finished"
-                    ).length
-                }
-            />
-            <PlayerStatus
-                name={playersState[1].name}
-                diceResult={playersState[1].diceResult}
-                color={playersState[1].color}
-                finishedCount={
-                    playersState[1].pawnsState.filter(
-                        (pawn) => pawn.position === "finished"
-                    ).length
-                }
-            />
+            <PlayerStatus playerIndex={0} />
+            <PlayerStatus playerIndex={1} />
             <DiceBuilder
                 duration={20}
                 maxSum={DEFAULTSUM + playersState[0].bonus}
